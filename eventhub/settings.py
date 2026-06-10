428e1b4499c9c7f826cddb3843dbfd287d84a10b
Harshita <51989304+harshitha-999@users.noreply.github.com>
2026-06-10 19:32:29 +0530

    Update SECRET_KEY to empty string for security
    Replaced the production secret key with an empty string for security.

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*cta#sv8xb!@+_92-gz*a(mu2rm7*svi1k93*e*!@e+3quz1g&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
